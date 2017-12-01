d57ff2b8da6a337ac798166adf6a573c11b1d9a4
Fat-Llama <connorglennon@outlook.com>
2017-12-01 14:22:09 +0000

    Initial commit

---
+++ app/src/main/java/com/gmail/at/connorglennon/weekendassignment3/MainActivity.java
@@ -0,0 +1,23 @@
+package com.gmail.at.connorglennon.weekendassignment3;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import butterknife.BindView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.navigation)
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
